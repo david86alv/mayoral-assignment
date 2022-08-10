3dff85162a55c3c48ad16f1bd4215fd789d5d2ee
David Alvarez <david.alvarez@seidor.es>
2022-08-10 19:21:49 +0200

    Agregada lógica de Test unitarios

--- src/components/ProductItem.test.js
+++ src/components/ProductItem.test.js
@@ -1,16 +1,38 @@
 import React from "react";
 import '@testing-library/jest-dom/extend-expect'
-import {render} from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react';
 import { ProductItem } from "./ProductItem";
 
-test('', () => {
-    const product = {
-        id: 10,
-        name: 'This is a test',
-        price: 9.99
-    }
+const productTest = { id: 0, name: 'camisa', price: 20, salePrice: 18, discount: 10, image: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' };
 
-    const component = render(<ProductItem product={product} />)
+test('Name Test', () => {
+    render(<ProductItem product={productTest} />);
+    const name = screen.getAllByText(productTest.name);
+    expect(name).toHaveLength(2);
+});
 
-    console.log(component)
-})
+test('Add cart Test', () => {
+    const productItem = render(<ProductItem product={productTest} />);
+    const button = productItem.container.querySelector('button');
+    screen.getByText('AÑADIR');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('type', 'button');
+});
+
+test('Image Test', () => {
+    const onError = jest.fn();
+    const productItem = render(<ProductItem product={productTest} />);
+    const image = productItem.container.querySelector('img');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', productTest.image);
+});
+
+test('No image Test', () => {
+    const onError = jest.fn();
+    const productItem = render(<ProductItem product={productTest} />);
+    const image = productItem.container.querySelector('img');
+    fireEvent.load(image);
+    fireEvent.error(image);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', '/images/no_image_web.png');
+});
