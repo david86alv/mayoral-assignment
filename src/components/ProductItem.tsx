894dd98ff8de9d27770ece15572971677c0411d6
david86alv <110994972+david86alv@users.noreply.github.com>
2022-08-12 13:58:37 +0200

    Eliminado botón

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -50,7 +50,6 @@ export const ProductItem = ({ product }) => {
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
             </div>
           </div>
         </div>
